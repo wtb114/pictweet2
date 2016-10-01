69bba60299909cf2c765d90fff3132b96c005d82
志田原健吾 <kengoshidahara@hotmail.com>
2016-10-01 16:50:54 +0900

    編集機能追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,14 +2,28 @@ class TweetsController < ApplicationController
   before_action :move_to_index, except: :index
 
   def index
-    @tweets = Tweet.order("created_at DESC").page(params[:page]).per(5)
+    @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(5)
   end
 
   def new
   end
 
   def create
-    Tweet.create(name: tweet_params[:name], image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+    Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy if tweet.user_id == current_user.id
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params) if tweet.user_id == current_user.id
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
   end
 
   def move_to_index
@@ -18,7 +32,7 @@ class TweetsController < ApplicationController
 
   private
   def tweet_params
-    params.permit(:name, :image, :text)
+    params.permit(:image, :text)
   end
 
 end
---
+++ db/migrate/20161001071038_remove_nickname_from_tweets.rb
@@ -0,0 +1,5 @@
+class RemoveNicknameFromTweets < ActiveRecord::Migration
+  def change
+    remove_column :tweets, :name, :string
+  end
+end
