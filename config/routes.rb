374fcb187b09a02f4725fe3d522c65759c8ef8d8
志田原健吾 <kengoshidahara@hotmail.com>
2016-10-01 18:32:08 +0900

    AAAA

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw  do
   devise_for :users
-root 'tweets#index'
-get 'tweets' => 'tweets#index'
-get 'tweets/new' => 'tweets#new'
-post 'tweets' => 'tweets#create'
-delete 'tweets/:id' => 'tweets#destroy'
-patch 'tweets/:id' => 'tweets#update'
-get 'tweets/:id/edit' => 'tweets#edit'
-get 'users/:id' => 'users#show'
+  root 'tweets#index'
+  resources :tweets do
+    resources :comments, only: [:create]
+  end
+  resources :users, only: [:show]
+
+# get 'tweets' => 'tweets#index'
+# get 'tweets/new' => 'tweets#new'
+# post 'tweets' => 'tweets#create'
+# delete 'tweets/:id' => 'tweets#destroy'
+# patch 'tweets/:id' => 'tweets#update'
+# get 'tweets/:id/edit' => 'tweets#edit'
+# get 'tweets/:id' => 'tweets#show'
+
+# get 'users/:id' => 'users#show'
 end
